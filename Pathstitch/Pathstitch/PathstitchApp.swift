82666a78699ce3d4c18ecb83cbcfcc9091476c77
Mason Chen <chen@masons-mbp.lan>
2026-06-03 15:01:42 -0400

    feat: initial project scaffold

---
+++ Pathstitch/Pathstitch/PathstitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathstitchApp.swift
+//  Pathstitch
+//
+//  Created by Mason Chen on 6/3/26.
+//
+
+import SwiftUI
+
+@main
+struct PathstitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pathstitch/PathstitchCLITest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  PathstitchCLITest
+//
+//  Created by Mason Chen on 6/3/26.
+//
+
+import Foundation
+
+print("Hello, World!")
+
